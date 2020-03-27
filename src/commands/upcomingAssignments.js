b5dbaa4dd80e903a77ed9158c2197367c593fd42
Dimitry Maizik <dima.m99@gmail.com>
2020-03-27 15:30:43 -0700

    Did some style changes. Fixes some bugs

--- src/commands/upcomingAssignments.js
+++ src/commands/upcomingAssignments.js
@@ -1,31 +1,35 @@
+const { google } = require('googleapis');
+require('dotenv').config();
+
 module.exports = async (msg) => {
-    const {google} = require('googleapis');
-    require('dotenv').config();
-    const GOOGLE_API = process.env.GOOGLE_API_KEY;
-    var response = "";
+  const GOOGLE_API = process.env.GOOGLE_API_KEY;
+  let response = '';
 
-    const calendar = google.calendar({version: 'v3', auth: GOOGLE_API});
-    calendar.events.list({
-    calendarId: '467isok03ftm8kq343is1b4jfg@group.calendar.google.com',
-    timeMin: (new Date()).toISOString(),
-    maxResults: 5,
-    singleEvents: true,
-    orderBy: 'startTime',
-    }, async (err, res) => {
+  const calendar = google.calendar({ version: 'v3', auth: GOOGLE_API });
+  calendar.events.list(
+    {
+      calendarId: '467isok03ftm8kq343is1b4jfg@group.calendar.google.com',
+      timeMin: new Date().toISOString(),
+      maxResults: 5,
+      singleEvents: true,
+      orderBy: 'startTime',
+    },
+    async (err, res) => {
+      if (err) return console.log(`The API returned an error: ${err}`);
+      const events = res.data.items;
 
-    if (err) return console.log('The API returned an error: ' + err);
-    const events = res.data.items;
-    
-    if (events.length) {
-        console.log('Upcoming 5 events:');
+      if (events.length) {
+        response += ('Upcoming 5 events: \n');
         events.map((event, i) => {
-        const start = event.start.dateTime.substring(0, 10);
-        //console.log(`${start} - ${event.summary}`);
-        response += `${start} - ${event.summary}\n`
+          const start = event.start.dateTime.substring(0, 10);
+          // console.log(`${start} - ${event.summary}`);
+          response += `${start} - ${event.summary}\n`;
         });
         await msg.channel.send(response);
-    } else {
-        console.log('No upcoming events found.');
-    }
-    });
+      } else {
+        response += ('No upcoming events found.');
+        await msg.channel.send(response);
+      }
+    },
+  );
 };
