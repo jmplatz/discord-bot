f1b87a1c78237e1f877a8a7f5be3304ec58d2e43
Dimitry <dima.m99@gmail.com>
2020-09-20 11:00:03 -0700

    Shows next 7 days of assignments

--- src/commands/upcomingAssignments.js
+++ src/commands/upcomingAssignments.js
@@ -4,13 +4,15 @@ require('dotenv').config();
 module.exports = async (msg) => {
   const GOOGLE_API = process.env.GOOGLE_API_KEY;
   let response = '';
-
+  let endDate = new Date();
+  date.setDate(date.getDate() + 7);
+  endDate = endDate.toISOString();
   const calendar = google.calendar({ version: 'v3', auth: GOOGLE_API });
   calendar.events.list(
     {
       calendarId: '467isok03ftm8kq343is1b4jfg@group.calendar.google.com',
       timeMin: new Date().toISOString(),
-      maxResults: 8,
+      timeMax: endDate,
       singleEvents: true,
       orderBy: 'startTime',
     },
@@ -19,7 +21,7 @@ module.exports = async (msg) => {
       const events = res.data.items;
 
       if (events.length) {
-        response += ('Upcoming 8 Assignments: \n');
+        response += ('Upcoming 7 days of Assignments: \n');
         events.map((event, i) => {
           const start = event.start.dateTime.substring(0, 10);
           // console.log(`${start} - ${event.summary}`);
@@ -27,7 +29,7 @@ module.exports = async (msg) => {
         });
         await msg.channel.send(response);
       } else {
-        response += ('No upcoming events found.');
+        response += ('No events in the next 7 days found.');
         await msg.channel.send(response);
       }
     },
