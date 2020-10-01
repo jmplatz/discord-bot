6b1a7b01974d49747945b40909ecc55bad812e25
Dimitry <dima.m99@gmail.com>
2020-10-01 11:12:32 -0700

    Detect Join / Left / Switched VC

--- src/vcManagement/index.js
+++ src/vcManagement/index.js
@@ -7,9 +7,17 @@ module.exports = async (oldMember, newMember) => {
     const newUserChannel = newMember.voiceChannel;
     const oldUserChannel = oldMember.voiceChannel;
 
-    console.log(oldMember);
-    console.log('--------------------------------');
-    console.log(newMember);
+    // console.log(oldMember);
+    // console.log('--------------------------------');
+    // console.log(newMember);
 
+    // Sometimes it's null, sometimes it's undefined... Why!!!!!!??????
+    if ((oldUserChannel.channelID === undefined || oldUserChannel.channelID === null) && (newUserChannel.channelID !== undefined || newUserChannel.channelID !== null)) {
+      console.log('Joined a Channel');
+    } else if ((oldUserChannel.channelID !== undefined || oldUserChannel.channelID !== null) && (newUserChannel.channelID === null || newUserChannel.channelID === undefined)) {
+      console.log('Left a Channel');
+    } else if ((oldUserChannel.channelID !== undefined || oldUserChannel.channelID !== null) && (newUserChannel.channelID !== undefined || newUserChannel.channelID !== null)) {
+      console.log('Switched Channel');
+    }
   }
 };
