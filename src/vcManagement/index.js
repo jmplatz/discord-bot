030147cb5a60a5870e49b1220fb5f1962513b5c0
Dimitry <dima.m99@gmail.com>
2020-10-01 10:24:03 -0700

    Imported Env Variables (woops!)

--- src/vcManagement/index.js
+++ src/vcManagement/index.js
@@ -1,3 +1,6 @@
+require('dotenv').config();
+const guildID = process.env.GUILD_ID;
+
 module.exports = async (oldMember, newMember) => {
   if (oldMember.guild.id === guildID || newMember.guild.id === guildID) {
     let newUserChannel = newMember.voiceChannel;
