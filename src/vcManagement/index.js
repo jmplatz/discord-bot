0870b6d071eca2b7c5060d1f0a8be9aad732b04a
Dimitry <dima.m99@gmail.com>
2020-10-01 11:24:53 -0700

    AHHHH

--- src/vcManagement/index.js
+++ src/vcManagement/index.js
@@ -4,19 +4,17 @@ const guildID = process.env.GUILD_ID;
 
 module.exports = async (oldMember, newMember) => {
   if (oldMember.guild.id === guildID || newMember.guild.id === guildID) {
-    const newUserChannel = newMember.voiceChannel;
-    const oldUserChannel = oldMember.voiceChannel;
 
     // console.log(oldMember);
     // console.log('--------------------------------');
     // console.log(newMember);
 
     // Sometimes it's null, sometimes it's undefined... Why!!!!!!??????
-    if ((oldUserChannel.channelID === undefined || oldUserChannel.channelID === null) && (newUserChannel.channelID !== undefined || newUserChannel.channelID !== null)) {
+    if ((oldMember.voice.channelID === undefined || oldMember.voice.channelID === null) && (newMember.channelID !== undefined || newMember.voice.channelID !== null)) {
       console.log('Joined a Channel');
-    } else if ((oldUserChannel.channelID !== undefined || oldUserChannel.channelID !== null) && (newUserChannel.channelID === null || newUserChannel.channelID === undefined)) {
+    } else if ((oldMember.voice.channelID !== undefined || oldMember.voice.channelID !== null) && (newMember.channelID === null || newMember.voice.channelID === undefined)) {
       console.log('Left a Channel');
-    } else if ((oldUserChannel.channelID !== undefined || oldUserChannel.channelID !== null) && (newUserChannel.channelID !== undefined || newUserChannel.channelID !== null)) {
+    } else if ((oldMember.voice.channelID !== undefined || oldMember.channelID !== null) && (newMember.voice.channelID !== undefined || newMember.voice.channelID !== null)) {
       console.log('Switched Channel');
     }
   }
