aab403175b88f9802ec1ef0ada6ae7aabe584008
Dima <Dima.m99@gmail.com>
2020-10-07 15:10:19 -0700

    Ready for first test-run

--- src/commands/commandlist.js
+++ src/commands/commandlist.js
@@ -3,7 +3,8 @@ const commands = new Map([
   ['!ping', 'icsBot will reply back with pong.'],
   ['!8ball', '!8ball < insert question > --> icsBot will respond back with an answer.'],
   ['!getgif', 'icsBot will respond back with a random gif!'],
-  ['!upcomingduedates', 'Prints out the next 5 assignments due.'],
+  ['!upcomingduedates', 'Prints out the next 7 days of assignments.'],
+  ['!addevent', '<!addevent> yyyy-mm-dd event name'],
   ['!uptime', 'icsBot will tell you how long it\'s been running.'],
 ]);
 
