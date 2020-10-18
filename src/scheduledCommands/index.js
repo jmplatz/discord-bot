3e20f6d2f40498d88657df68345442fa82d8eaf8
Dima <Dima.m99@gmail.com>
2020-10-17 22:24:10 -0700

    Mention Please!

--- src/scheduledCommands/index.js
+++ src/scheduledCommands/index.js
@@ -89,11 +89,12 @@ module.exports = async () => {
         }
 
         const foundEmbed = new indexData.discord.MessageEmbed();
-        foundEmbed.setTitle('Good Morning! <@152987947098963977> ');
+        foundEmbed.setTitle('Good Morning!');
         foundEmbed.addField("Due Today: ", dayOneOutput , false);
         foundEmbed.addField("Due Tomorrow: ", dayTwoOutput , false);
         foundEmbed.setTimestamp();
         foundEmbed.setFooter('Something Missing? !addDueDate 2020-12-31 Assignment Title');
+        await indexData.client.channels.cache.get("753718038280536064").send("<@152987947098963977> ");
         await indexData.client.channels.cache.get("753718038280536064").send(foundEmbed);
 
         } else {
