5ee3258f0ac65fc413a517e70b3ef692595e931c
Dimitry <dima.m99@gmail.com>
2020-11-06 15:52:47 -0800

    Was using wrong arg

--- src/commands/upcomingAssignments.js
+++ src/commands/upcomingAssignments.js
@@ -22,8 +22,7 @@ function getMonthTextual(date){
 module.exports = async (msg, args) => {
   const GOOGLE_API = process.env.GOOGLE_API_KEY;
   let dayModifier = 7;
-  console.log(args);
-  if(args[1] == undefined){
+  if(args[0] == undefined){
     // Don't need to do anything
   }else if(isNaN(args[1])){
     await msg.channel.send("Invalid Date Modifier, Assuming 7 Days.");
