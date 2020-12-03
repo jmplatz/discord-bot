731a5c0cd3641d842de40075bace1325a30fa17e
Dima <dima.m99@gmail.com>
2020-12-03 14:09:32 -0800

    Cursed functionality Part 8

--- src/commands/uwu.js
+++ src/commands/uwu.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 const indexData = require("../index");
 const UWU = process.env.UWUID;
+const icsBotID = process.env.BOTID;
 
 module.exports = async (msg, args) => {
   if (msg.channel.id !== UWU) return;
+  if (msg.author.id === icsBotID) return;
 
   message = "";
   for (var i = 0; i < args.length; i++) {
@@ -15,8 +17,8 @@ module.exports = async (msg, args) => {
 
   // stack overflow stuff
   //await indexData.client.channels.cache.get(args[0].toString()).send(mess);
-  msg.channel.send(returnMessage);
-  msg.delete();
-  
+  msg.channel.send(returnMessage).then(() => {
+    msg.delete();
+  })
   
 };
