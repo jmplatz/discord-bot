a376bfd80ed952392c262a422a4d5533473e301e
Dima <dima.m99@gmail.com>
2021-01-12 13:18:45 -0800

    Pin Message on 📌 cleanup

--- src/reactionManagement/index.js
+++ src/reactionManagement/index.js
@@ -17,8 +17,5 @@ module.exports = async (reaction, user) => {
     if(reaction.emoji.name === '📌' && reaction.count === 1) {
         await reaction.message.pin({reason: 'Pinned by ' + reaction.message.author});
     }
-	// Now the message has been cached and is fully available
-	console.log(`${reaction.message.author}'s message "${reaction.message.content}" gained a reaction!`);
-	// The reaction is now also fully available and the properties will be reflected accurately:
-	console.log(`${reaction.count} user(s) have given the same reaction to this message!`);
+	
 }
